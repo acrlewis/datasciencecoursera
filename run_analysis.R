f15bbcf522d56aa278d627e3c7729f174cd4bf14
clewis1 <acrlewis1@gmail.com>
2014-06-11 14:08:00 -0700

    test 6

--- run_analysis.R
+++ run_analysis.R
@@ -1,6 +1,6 @@
 # read the column names
-data_cols <- read.table("features.txt", header=FALSE, as.is=TRUE, col.names=c("measureid", "measurename"))
-  
+data_cols <- read.table("features.txt", header=FALSE, col.names=c("measureid", "measurename"))
+ 
 subject_data <- read.table("train/subject_train.txt", header=FALSE, col.names=c("subjectid"))
 subject_data <- rbind(subject_data, read.table("test/subject_test.txt", header=FALSE, col.names = c("subjectid")))
   
@@ -15,14 +15,17 @@ x_datanew <- x_datanew[,subset_data_cols]
 x_data <- rbind(x_data, x_datanew)
 
 # read the y data file
-y_data <- read.table("train/y_train.txt", header=FALSE, col.names=c("activityid"))
+y_data <- read.table("train/y_train.txt", header = FALSE, col.names = c("activity"))
 y_data <- rbind(y_data, read.table("test/y_test.txt", header=FALSE, col.names = c("activityid")))
-  
-activity_labels <- read.table("activity_labels.txt", header=FALSE, as.is=TRUE, col.names=c("activityid", "activityname"))
+ 
+
+
+activity_labels <- read.table("activity_labels.txt", header=FALSE, col.names=c("activity", "activityname"))
 activities <- activity_labels[y_data$activity,]$activityname
-  
+
+
 # create a tidy data set that has the average of each variable for each activity and each subject.
-tidy_data <- cbind(subject_data, activites, x_data)
+tidy_data <- cbind(subject_data, activities, x_data)
 tidy_data.avg <- aggregate(tidy[, 3:dim(tidy)[2]], list(tidy$subject, tidy$activity), mean)
 
 
