a856f1ce192f01f421ce2a086101e19a329dac82
clewis1 <acrlewis1@gmail.com>
2014-06-11 13:35:56 -0700

    test 3

--- run_analysis.R
+++ run_analysis.R
@@ -1,9 +1,41 @@
-## Getting and Cleaning Data Course Project
+# read the column names
+data_cols <- read.table("features.txt", header=FALSE, as.is=TRUE, col.names=c("measureid", "measurename"))
+  
+subject_data <- read.table("train/subject_train.txt", header=FALSE, col.names=c("subjectid"))
+subject_data <- rbind(subject_data, read.table("test/subject_test.txt", header=FALSE, col.names = c("subjectid")))
+  
+# names of subset columns required
+subset_data_cols <- grep(".*mean\\(\\)|.*std\\(\\)", data_cols$measurename)
+  
+# read the x data file
+x_data <- read.table("test/x_test.txt", header=FALSE, col.names=data_cols$measurename)
+x_data <- x_data[,subset_data_cols]
+x_datanew <- read.table("train/x_train.txt", header=FALSE, col.names=data_cols$measurename)
+x_datanew <- x_datanew[,subset_data_cols]
+x_data <- rbind(x_data, x_datanew)
 
-## You should create one R script called run_analysis.R that does the following. 
-run_analysis <- function
-## Merges the training and the test sets to create one data set.
-## Extracts only the measurements on the mean and standard deviation for each measurement. 
-## Uses descriptive activity names to name the activities in the data set
-## Appropriately labels the data set with descriptive variable names. 
-## Creates a second, independent tidy data set with the average of each variable for each activity and each subject. 
+# read the y data file
+y_data <- read.table("train/y_train.txt", header=FALSE, col.names=c("activityid"))
+y_data <- rbind(y_data, read.table("test/y_test.txt", header=FALSE, col.names = c("activityid")))
+  
+activity_labels <- read.table("activity_labels.txt", header=FALSE, as.is=TRUE, col.names=c("activityid", "activityname"))
+y_data$activity <- activity_labels[y_data$activity,]$activityname
+  
+# subset the data (done early to save memory)
+data_labels <- merge(y_data, activity_labels)
+  
+# create a tidy data set that has the average of each variable for each activity and each subject.
+tidy_data <- cbind(subset_data_cols, y_data, x_data)
+tidy_data.avg <- aggregate(tidy[, 3:dim(tidy)[2]], list(tidy$subject, tidy$activity), mean)
+names(tidy_data.avg)[1:2] <- c('subject', 'activity')
+
+# create the tidy data set and tidy data avg and save it on to the named files
+print("creating tidy dataset as tidy.csv...")
+write.csv(tidy_data,"tidy.csv")
+print("done.")
+
+print("creating tidy dataset as tidy.csv...")
+write.csv(tidy_data.avg,"tidy_avg.csv")
+print("done.")
+
+print("Tidy complete.")
