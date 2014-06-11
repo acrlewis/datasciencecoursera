08f03cd00822e8143c566384b1cd0d8ad787c28f
clewis1 <acrlewis1@gmail.com>
2014-06-11 14:59:48 -0700

    test 7

--- run_analysis.R
+++ run_analysis.R
@@ -1,39 +1,44 @@
-# read the column names
-data_cols <- read.table("features.txt", header=FALSE, col.names=c("measureid", "measure"))
- 
-subject_data <- read.table("train/subject_train.txt", header=FALSE, col.names=c("subject"))
-subject_data <- rbind(subject_data, read.table("test/subject_test.txt", header=FALSE, col.names = c("subject")))
+# read all the files in train
+# add cnames
+# add two additional "ID" columns
+# - first from subject ID
+# - second from activity ID
+# do same for all test files
+# merge train with test
+# ensure tidy_data data
+#
 
-# names of subset columns required
-subset_data_cols <- grep(".*mean\\(\\)|.*std\\(\\)", data_cols$measure)
-  
-# read the x data file
-x_data <- read.table("test/x_test.txt", header=FALSE, col.names=data_cols$measure)
-x_data <- x_data[,subset_data_cols]
-x_datanew <- read.table("train/x_train.txt", header=FALSE, col.names=data_cols$measure)
-x_datanew <- x_datanew[,subset_data_cols]
-x_data <- rbind(x_data, x_datanew)
+# read the files
+# assign the headers
 
-# read the y data file
-y_data <- read.table("train/y_train.txt", header = FALSE, col.names = c("activity"))
-y_data <- rbind(y_data, read.table("test/y_test.txt", header=FALSE, col.names = c("activity")))
+# grab the headers out of the "features.txt" file
+data_cols <- read.table("features.txt", header=F, col.names=c("measureid", "measure"))
 
-activity_labels <- read.table("activity_labels.txt", header=FALSE, col.names=c("activity", "activityname"))
-activities <- activity_labels[y_data$activity,]$activityname
+# read and set columns for subject_data
+subject_data <- read.table("train/subject_train.txt", header = F, col.names = c("subject"))
+subject_data <- rbind(subject_data, read.table("test/subject_test.txt", header = F, col.names = c("subject")))
 
-# create a tidy data set that has the average of each variable for each activity and each subject.
-tidy_data <- cbind(subject_data, activities, x_data)
-tidy_data.avg <- aggregate(tidy_data[, 3:dim(tidy_data)[2]], list(tidy_data$subject, tidy_data$activity), mean)
+# read and set columns for x_data
+subset_cols <- grep(".*mean\\(\\)|.*std\\(\\)", data_cols$measure)
+x_data <- read.table("train/X_train.txt", header = F, col.names = data_cols$measure)
+x_data <- x_data[,subset_cols]
+x_data_new <- read.table("test/X_test.txt", header = F, col.names = data_cols$measure)
+x_data_new <- x_data_new[,subset_cols]
+x_data <- rbind(x_data, x_data_new)
 
-names(tidy_data.avg)[1:2] <- c('subject', 'activity')
+# read and set columns for y_data
+y_data <- read.table("train/y_train.txt", header = F, col.names = c("activity"))
+y_data <- rbind(y_data, read.table("test/y_test.txt", header = F, col.names = c("activity")))
 
-# create the tidy data set and tidy data avg and save it on to the named files
-print("creating tidy dataset as tidy.csv...")
-write.csv(tidy_data,"tidy.csv")
-print("done.")
+# read and set activity labels
+activity_labels <- read.table("activity_labels.txt", header=F, col.names=c("activity", "activityname"))
+y_data$activity <- activity_labels[y_data$activity,]$activityname
 
-print("creating tidy dataset as tidy.csv...")
-write.csv(tidy_data.avg,"tidy_avg.csv")
-print("done.")
+# Produce the tidy_data data
+tidy_data <- cbind(subject_data, y_data, x_data)
+write.csv(tidy_data, "tidy_data.csv")
 
-print("Tidy complete.")
+tidy_data_avg <- aggregate(tidy_data[,3:dim(tidy_data)[2]],list(tidy_data$subject,tidy_data$activity), mean)
+
+names(tidy_data_avg)[1:2]<-c('subject', 'activity')
+write.csv(tidy_data_avg, "tidy_data_avg.csv")
